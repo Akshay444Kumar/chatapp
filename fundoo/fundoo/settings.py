3d0c0fe79c453285076ea1e6b4b5029a21ccf0f5
Akshay Kumar <120436185+Akshay444Kumar@users.noreply.github.com>
2024-07-03 13:40:14 +0530

    Update settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -104,9 +104,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CHATDB',
-        'USER': 'CHATDBUSER',
-        'PASSWORD': 'CHATDBPASSWORD',
-        'HOST': 'CHATDBHOST',
+        'USER': 'CHATUSER',
+        'PASSWORD': 'CHATPASSWORD',
+        'HOST': 'CHATHOST',
         'PORT': '3306',
     }
 }
